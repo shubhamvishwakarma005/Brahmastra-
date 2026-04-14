78cbb86f76d062bc6c2158f0b4b4f50deea95015
Shubham <vishwakarmashubham202@gmail.com>
2026-04-14 13:20:53 +0530

    done revision array

--- Frontend/js-collections/array.js
+++ Frontend/js-collections/array.js
@@ -45,22 +45,56 @@ console.log();
 // **************************OPERATIONS**********************************
 
 const order = [
-    { dish: "pasta", price: 399, qty: 4 },
-    { dish: "rasam", price: 249, qty: 2 },
-    { dish: "idli", price: 149, qty: 8 },
-    { dish: "vada", price: 149, qty: 8 },
-    { dish: "samosa", price: 49, qty: 12 },
+    { dish: "pasta", price: 399, qty: 4 , spicy:true},
+    { dish: "rasam", price: 249, qty: 2 , spicy:true},
+    { dish: "idli", price: 149, qty: 8 , spicy:false},
+    { dish: "vada", price: 149, qty: 8 , spicy:false},
+    { dish: "samosa", price: 49, qty: 12 , spicy:true},
 ]
 
+// forEach() || forEach() accept sync function, it do not work in async
 order.forEach( (order, index)=>{
-    // console.log(`${index + 1}: ${order}`)  // string
+    // console.log(`${index + 1}: ${order}`)  // string 
 } )
 
-
+// map( )
 const myDish = order.map((ord)=>(`I Ordered ${ord.qty} ${ord.dish}`))
-// console.log(myDish) // array
+// console.log(myDish) // array []
 
-
+// filter()
 const dishPrice = order.filter((ord)=>(ord.price >= 200 ))
-// console.log(dishPrice) // array of object
+// console.log(dishPrice) // array of object [{},{}]
 
+// reduce() #1 
+const serveCharge = order.reduce((accum, currVal)=>(accum + (currVal.price * currVal.qty)), 20)
+// console.log(serveCharge)
+
+// #2
+const checkSpicy = order.reduce((accum, ord)=>{
+     const category =  ord.spicy ? "spicy": "mild"
+     accum[category].push(ord.dish)
+     return accum
+}, {spicy:[], mild:[]})
+
+// console.log(checkSpicy)  // object of array {[],[]}
+
+// eg: accum[category] = accum.spicy || accum.mild =  accum[category]
+
+// #3 
+const checkPrice = order.reduce((accum, ord)=>{
+const prices = ord.price >= 200 ? "expencive" : "cheap"
+accum[prices].push(ord.dish)
+return accum
+},{expencive:[], cheap:[]})
+
+// console.log(checkPrice)  // object of array {[], []}
+
+
+// sorting 
+const nums = [20, 50,32, 4, 56, 82]
+const assendSort = [...nums].sort((a,b)=> a-b)  // assending order
+const decendSort = [...nums].sort((a,b)=> b-a)
+// console.log(assendSort)
+// console.log(decendSort)
+
+// console.log( nums.toSorted((a,b) =>(a-b)) )
