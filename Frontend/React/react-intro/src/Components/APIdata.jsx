7232fe4e28e7b184bba3aa3365b414b029835049
Shubham <vishwakarmashubham202@gmail.com>
2026-05-08 22:42:10 +0530

    learn some new concepts like callback hell and learning fetching data from api

--- Frontend/React/react-intro/src/Components/APIdata.jsx
+++ Frontend/React/react-intro/src/Components/APIdata.jsx
@@ -1,64 +1,15 @@
-import { useEffect, useState } from "react";
+// api data 
+// const API_URL = 'https://api.example.com/data';
 
-function RandomUsers() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
 
-  const url = "https://api.freeapi.app/api/v1/public/randomusers/13";
+function APIdata() {
 
-  useEffect(() => {
-    async function fetchUsers() {
-      try {
-        const res = await fetch(url);
-        const data = await res.json();
-
-        // 👇 API structure check kar (important)
-        setUsers(data?.data?.data || []);
-        setLoading(false);
-      } catch (err) {
-        console.error(err);
-        setLoading(false);
-      }
-    }
-
-    fetchUsers();
-  }, []);
-
-  const styles = {
-  container: {
-    display: "grid",
-    gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
-    gap: "20px",
-    padding: "20px"
-  },
-  card: {
-    background: "#fff",
-    padding: "15px",
-    borderRadius: "10px",
-    boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
-    textAlign: "center"
-  },
-  image: {
-    borderRadius: "50%",
-    width: "100px",
-    height: "100px"
-  }
-};
-
-  if (loading) return <h2>Loading...</h2>;
 
   return (
-    <div style={styles.container}>
-      {users.map((user, index) => (
-        <div key={index} style={styles.card}>
-          <img src={user.picture.large} alt="user" style={styles.image} />
-          <h3>{user.name.first} {user.name.last}</h3>
-          <p>{user.email}</p>
-          <p>{user.location.city}, {user.location.country}</p>
-        </div>
-      ))}
+    <div>
+      <h2>API Data Component</h2>
     </div>
-  );
+  )
 }
 
-export default RandomUsers;
+export default APIdata
---
+++ Frontend/js-collections/CallbackHell.js
@@ -0,0 +1,14 @@
+// function CreateOrder(){
+//     showOrder(){
+//         UpdateWallet(){
+//             console.log('task done')
+//         }
+//     }
+// }
+
+
+// so as we see that each task is dependent to other task , if any fails whole application got crashed , this is called "Callback hell".
+
+// issue of callback
+// #  Callback Hell  
+// # inversion of control
